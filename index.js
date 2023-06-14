563ef742dae256a96cccd7ee1a471354a3b2cb56
Neeltech92 <115792985+Neeltech92@users.noreply.github.com>
2023-06-14 07:24:00 +0000

    rtertet

--- index.js
+++ index.js
@@ -2,27 +2,59 @@ let hScore = document.getElementById("h-score")
 let gScore = document.getElementById("g-score")
 let hTotal = 0
 let gTotal = 0
-function Hadd1() {
-    hTotal += 1
-    hScore.innerText = hTotal
-}
-function Hadd2() {
-    hTotal += 2
-    hScore.innerText = hTotal
-}
-function Hadd3() {
-    hTotal += 3
-    hScore.innerText = hTotal
-}
-function Gadd1() {
-    gTotal += 1
-    gScore.innerText = gTotal
-}
-function Gadd2() {
-    gTotal += 2
-    gScore.innerText = gTotal
-}
-function Gadd3() {
-    gTotal += 3
-    gScore.innerText = gTotal
+
+// function Hadd1() {
+//     hTotal += 1
+//     hScore.innerText = hTotal
+// }
+// function Hadd2() {
+//     hTotal += 2
+//     hScore.innerText = hTotal
+// }
+// function Hadd3() {
+//     hTotal += 3
+//     hScore.innerText = hTotal
+// }
+// function Gadd1() {
+//     gTotal += 1
+//     gScore.innerText = gTotal
+// }
+// function Gadd2() {
+//     gTotal += 2
+//     gScore.innerText = gTotal
+// }
+// function Gadd3() {
+//     gTotal += 3
+//     gScore.innerText = gTotal
+// }
+
+document.addEventListener("click", (e) => {
+    calculate(e)
+})
+
+function calculate(event) {
+    if(event.target.id === "Hadd1") {
+        hTotal += 1
+        hScore.innerText = hTotal
+    } 
+    else if (event.target.id === "Hadd2") {
+        hTotal += 2
+        hScore.innerText = hTotal
+    }
+    else if (event.target.id === "Hadd3") {
+        hTotal += 3
+        hScore.innerText = hTotal
+     }
+    else if (event.target.id === "Gadd1") {
+        gTotal += 1
+        gScore.innerText = gTotal
+     }
+    else if (event.target.id === "Gadd2") {
+        gTotal += 2
+        gScore.innerText = gTotal
+     }
+    else if (event.target.id === "Gadd3") {
+        gTotal += 3
+        gScore.innerText = gTotal
+     }
 }
